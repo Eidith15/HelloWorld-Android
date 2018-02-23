45f62d543c3dcbd30421f135c27f147f6fdfed02
Adhitya Sidiq Permana <adhityasp18@gmail.com>
2018-02-23 13:14:12 +0700

    upload tugas 1 Hello World

---
+++ app/src/main/java/sab/tugas1/eidith/helloworld/Tampil_HelloWorld.java
@@ -0,0 +1,13 @@
+package sab.tugas1.eidith.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tampil_HelloWorld extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampil__hello_world);
+    }
+}
